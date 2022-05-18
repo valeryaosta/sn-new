fc1e53defc1b5434c3599392298ad836cbecb494
valeryaosta <gerasimenko.valery@gmail.com>
2022-05-18 13:07:48 +0300

    21 - Dialogs page markup

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
-// import s from './Dialogs.moodule.css'
+import s from './Dialogs.module.css'
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Valery
+                </div>
+                <div className={s.dialog}>
+                    Dmitry
+                </div>
+                <div className={s.dialog}>
+                    Kseniya
+                </div>
+                <div className={s.dialog}>
+                    Vitaliy
+                </div>
+                <div className={s.dialog}>
+                    Vladislav
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hello wooooah here!</div>
+                <div className={s.message}>I don’t care what you think about me. I don’t think about you at all.</div>
+                <div className={s.message}>Life is a series of choices.</div>
+            </div>
         </div>
     );
 };
