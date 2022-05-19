d8b2792396c9c5c7fcbc61294fae782f84163006
valeryaosta <gerasimenko.valery@gmail.com>
2022-05-19 16:30:56 +0300

    31 - ref

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import s from './Dialogs.module.css'
+import style from '../Profile/MyPosts/MyPosts.module.css'
 import {DialogItem} from "./DialogItem/DialogItem";
 import {Message} from "./Message/Message";
 
 
 const Dialogs = ({dialogs, messages}) => {
 
+    const textRef = useRef(null)
+
     const dialogsElements = dialogs.map((d) => <DialogItem id={d.id} name={d.name} key={d.id} ava={d.ava}/>)
 
     const messagesElements = messages.map((msg) => <Message message={msg.message} key={msg.id}/>)
+
+    const onAddPost = () => {
+        const newText = textRef.current.value
+        alert(newText)
+    }
 
     return (
         <div className={s.dialogs}>
@@ -17,7 +25,13 @@ const Dialogs = ({dialogs, messages}) => {
             </div>
             <div className={s.messages}>
                 {messagesElements}
+                <div className={s.messageArea}>
+                <textarea ref={textRef} placeholder="Add a new message.." cols="30" rows="5">
+                </textarea>
+                    <button onClick={onAddPost} className={`${style.btn} ${style.btn1}`}>Add post</button>
+                </div>
             </div>
+
         </div>
     );
 };
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import s from './MyPosts.module.css'
 import Post from "./Post/Post";
 
 const MyPosts = ({posts}) => {
 
+    const textareaRef = useRef(null)
+
     const postsElements = posts.map((post) => <Post key={post.id} message={post.message} likesCount={post.likesCount}/>)
+
+    const onAddPost = () => {
+        let text = textareaRef.current.value
+        alert(text)
+    }
+
+    const onRemovePost = () => {
+        alert('remove')
+    }
 
     return (
         <div className={s.postsBlock}>
             MyPosts
             <div className={s.wrapper}>
-            <textarea placeholder="Remember, be nice!" cols="30" rows="5">
+            <textarea ref={textareaRef} placeholder="Remember, be nice!" cols="30" rows="5">
             </textarea>
-                <button className={`${s.btn} ${s.btn1}`}>Add post</button>
-                <button className={`${s.btn} ${s.btn2}`}>Remove</button>
+                <button onClick={onAddPost} className={`${s.btn} ${s.btn1}`}>Add post</button>
+                <button onClick={onRemovePost} className={`${s.btn} ${s.btn2}`}>Remove</button>
             </div>
             <div className={s.postsWrapper}>
                 {postsElements}
