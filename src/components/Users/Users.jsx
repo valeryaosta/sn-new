48803f7eb3e347421e60016b1b2ed4eb4dfaba1a
valeryaosta <gerasimenko.valery@gmail.com>
2022-05-26 16:23:42 +0300

    51 - users API

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,23 +1,14 @@
 import React from 'react';
 import style from './Users.module.css'
+import axios from "axios";
 
 const Users = ({users, follow, unfollow, setUsers}) => {
 
+    const photoUrl = 'https://newsd.in/wp-content/uploads/2019/11/04d62c82df95ec3ff3a230c681b36a14.jpg'
+
     if (users.length === 0) {
-        setUsers([
-            {
-                id: 1, photo: 'https://newsd.in/wp-content/uploads/2019/11/04d62c82df95ec3ff3a230c681b36a14.jpg',
-                followed: false, fullName: 'Lera', status: 'Heeello', location: {city: 'Minsk', country: 'Belarus'}
-            },
-            {
-                id: 2, photo: 'https://newsd.in/wp-content/uploads/2019/11/04d62c82df95ec3ff3a230c681b36a14.jpg',
-                followed: false, fullName: 'Sveta', status: 'Heeello', location: {city: 'Kyiv', country: 'Ukraine'}
-            },
-            {
-                id: 3, photo: 'https://newsd.in/wp-content/uploads/2019/11/04d62c82df95ec3ff3a230c681b36a14.jpg',
-                followed: false, fullName: 'Michael', status: 'Heeello', location: {city: 'New York', country: 'USA'}
-            },
-        ])
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?count=10`)
+            .then(res => setUsers(res.data.items))
     }
 
     return (
@@ -26,7 +17,7 @@ const Users = ({users, follow, unfollow, setUsers}) => {
             <div className={style.usersBlock}>
                 {users.map((u) => <div key={u.id} className={style.userWrapper}>
                         <div className={style.user}>
-                            <img src={u.photo} alt='user'/>
+                            <img src={u.photos.small !== null ? u.photos.small : photoUrl} alt='user'/>
 
                             {u.followed
                                 ? <button onClick={() => unfollow(u.id)} className={`${style.customBtn} ${style.btn}`}>
@@ -42,14 +33,16 @@ const Users = ({users, follow, unfollow, setUsers}) => {
 
                         <div className={style.userInfo}>
                             <div className={style.leftSider}>
-                                <span className={style.boldText}>{u.fullName}</span>
+                                <span className={style.boldText}>{u.name}</span>
                                 <span className={style.status}>{u.status}</span>
                             </div>
                             <div className={style.rightSider}>
-                                <span className={style.boldText2}>{u.location.country},</span>
-                                <span className={style.boldText2}>{u.location.city}</span>
+                                <span className={style.boldText2}>{'Country'}</span>
+                                <span className={style.boldText2}>{'City'}</span>
                             </div>
                         </div>
+
+
                     </div>
                 )}
             </div>
